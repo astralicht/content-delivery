83858dc7f6e1080555793d98c51eaf1ce0e5e036
Julac Ontina <eneioarzew@gmail.com>
2023-03-03 11:54:31 +0800

    Sample custom components (Hover and click toggles)

--- public/pages/home.php
+++ public/pages/home.php
@@ -2,13 +2,115 @@
 $PAGE_TITLE = "Home";
 ?>
 
-<div flex="v">
-    <div flex="h" v-center padding>
-        <h3 no-margin>EneioArzew</h3>
-        <span text="medium">Content Delivery System</span>
+<style>
+    body {
+        background-color: var(--darker-color);
+        color: white;
+    }
+</style>
+
+<div flex="v" no-gap>
+    <div flex="h" v-center padding-wider primary white-text>
+        <span text="medium"><b>Content Delivery</b> Framework</span>
     </div>
-    <div flex="v" padding>
-        <a href="page">Go to another page</a>
-        <a href="another/page">Go to yet another page</a>
+    <div class="main-content" flex="v" padding-wider flex-grow="1">
+        <header>
+            <h1>🍌 banana 🍌</h1>
+        </header>
+        <section>
+            <div flex="h" flex-wrap>
+                <div class="card" dark padding-wider>
+                    <span text="medium">Banana</span>
+                    <img src="public/files/banana-rotate.gif" alt="banana" width="100%">
+                </div>
+                <div class="card" dark padding-wider>
+                    <span text="medium">Banana</span>
+                    <img src="public/files/banana-rotate.gif" alt="banana" width="100%">
+                </div>
+                <div class="card" dark padding-wider>
+                    <span text="medium">Banana</span>
+                    <img src="public/files/banana-rotate.gif" alt="banana" width="100%">
+                </div>
+                <div class="card" dark padding-wider>
+                    <span text="medium">Banana</span>
+                    <img src="public/files/banana-rotate.gif" alt="banana" width="100%">
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <ul>
+                <li>Hello</li>
+                <li>I am</li>
+                <li>Banana</li>
+            </ul>
+        </section>
+
+        <section flex="v">
+            <hover-toggle>
+                <title dark>
+                    Hover toggle
+                </title>
+                <container>
+                    <item dark>
+                        b a n a n a
+                    </item>
+                    <item dark>
+                        b a n a n a
+                    </item>
+                    <item dark>
+                        b a n a n a
+                    </item>
+                </container>
+            </hover-toggle>
+
+            <button class="click-toggle">
+                <title>
+                    Click toggle
+                </title>
+                <container>
+                    <item dark>
+                        b a n a n a
+                    </item>
+                    <item dark>
+                        b a n a n a
+                    </item>
+                    <item dark>
+                        b a n a n a
+                    </item>
+                </container>
+            </button>
+        </section>
+
+        <table dark-text>
+            <thead>
+                <th>Fruit</th>
+                <th>Fruit</th>
+                <th>Fruit</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Banana</td>
+                    <td>Banana</td>
+                    <td>Banana</td>
+                </tr>
+                <tr>
+                    <td>Banana</td>
+                    <td>Banana</td>
+                    <td>Banana</td>
+                </tr>
+                <tr>
+                    <td>Banana</td>
+                    <td>Banana</td>
+                    <td>Banana</td>
+                </tr>
+            </tbody>
+        </table>
+
+        <select name="" id="" dark-text>
+            <option value="">Banana</option>
+            <option value="">Banana</option>
+            <option value="">Banana</option>
+        </select>
     </div>
 </div>
