25d49c5c8158de5a26ff52bd9e4db4055d095f00
raph0ryte <julac_15@yahoo.com>
2023-05-16 23:49:46 +0800

    Updated Egg CSS version name.

--- App.php
+++ App.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= \cdf\Config::$DOCUMENT_ROOT ?>styles/egg_2-1.css">
+    <link rel="stylesheet" href="<?= \cdf\Config::$DOCUMENT_ROOT ?>styles/egg_v01-03.css">
     <link rel="stylesheet" href="<?= \cdf\Config::$DOCUMENT_ROOT ?>styles/normalize.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/js/all.min.js" integrity="sha512-naukR7I+Nk6gp7p5TMA4ycgfxaZBJ7MO5iC3Fp6ySQyKFHOGfpkSZkYVWV5R7u7cfAicxanwYQ5D1e17EfJcMA==" crossorigin="anonymous" referrerpolicy="no-referrer" defer></script>
     <title><?= $PAGE_TITLE . " | " . \cdf\Config::$APP_NAME; ?></title>
